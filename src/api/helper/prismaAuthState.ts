36abc12f460e9bfe8570c6252273a321babe709d
Git-RenatoAlcantara <renato.forjob@gmail.com>
2026-05-13 17:53:20 -0300

    refactor: use initAuthCreds from baileys instead of manual implementation

--- src/api/helper/prismaAuthState.ts
+++ src/api/helper/prismaAuthState.ts
@@ -1,28 +1,5 @@
-import {
-    proto,
-    Curve,
-    signedKeyPair,
-    generateRegistrationId,
-} from '@whiskeysockets/baileys'
-import { randomBytes } from 'crypto'
+import { proto, initAuthCreds } from '@whiskeysockets/baileys'
 import { prisma } from './prismaClient.js'
-
-const initAuthCreds = () => {
-    const identityKey = Curve.generateKeyPair()
-    return {
-        noiseKey: Curve.generateKeyPair(),
-        signedIdentityKey: identityKey,
-        signedPreKey: signedKeyPair(identityKey, 1),
-        registrationId: generateRegistrationId(),
-        advSecretKey: randomBytes(32).toString('base64'),
-        processedHistoryMessages: [] as any[],
-        nextPreKeyId: 1,
-        firstUnuploadedPreKeyId: 1,
-        accountSettings: {
-            unarchiveChats: false,
-        },
-    }
-}
 
 const BufferJSON = {
     replacer: (_k: string, value: any) => {
